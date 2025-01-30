56792830a9a93453150f380f73f4f6294bb4ae40
Danil <fofogogo12345689@mail.ru>
2025-01-30 12:45:09 +0500

    add class in product and fixed main

--- src/product.py
+++ src/product.py
@@ -46,3 +46,22 @@ class Product:
         if isinstance(other, Product):
             return self.price * self.quantity + other.price * other.quantity
         raise TypeError("Нельзя складывать объекты разных типов")
+
+
+class Smartphone(Product):
+    def __init__(self, name: str, description: str, price: float, quantity: int,
+                 efficiency: float, model: str, memory: int, color: str):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name: str, description: str, price: float, quantity: int,
+                 country: str, germination_period: str, color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
