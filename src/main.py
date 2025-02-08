55a486e171d344390087b49395f1ba5970524f5b
Danil <fofogogo12345689@mail.ru>
2025-02-08 09:26:25 +0500

    fixed main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+from src.category import Category
+from src.product import Product
+
 if __name__ == '__main__':
     try:
         product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
     except ValueError as e:
         print(
-            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
+            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством"
+        )
     else:
         print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
 
@@ -12,7 +16,6 @@ if __name__ == '__main__':
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
     category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
-
     print(category1.middle_price())
 
     category_empty = Category("Пустая категория", "Категория без продуктов", [])
